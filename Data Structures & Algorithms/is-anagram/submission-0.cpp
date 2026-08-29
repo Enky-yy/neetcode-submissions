fc86ef4c4e5212caff7cdbd2802d0ac31149822f
Harsh Shah <shahharsh4653@gmAIL.COM>
2026-08-29 18:39:11 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int k = t.size();
+        if(n!=k)
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for(int i =0 ; i<n;i++){
+            if(s[i]-t[i]!=0)
+                return false;
+        }
+        return true;
+    }
+};
